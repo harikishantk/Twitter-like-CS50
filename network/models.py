0f8d4a15df3340adb31ccb35707872e1802a3711
Harikishan <harikishantk@gmail.com>
2022-06-18 13:26:03 +0530

    db model schema defined

--- network/models.py
+++ network/models.py
@@ -8,4 +8,25 @@ class User(AbstractUser):
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
-    
+    created_on = models.DateField(auto_now_add=True)
+    updated_on = models.DateField(auto_now=True)
+    like_counts = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.description + ' - ' + str(self.user)
+
+class LikePost(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    liked_by = models.ManyToManyField(User)
+    created_on = models.DateField(auto_now_add=True)
+    updated_on = models.DateField(auto_now=True)
+
+    def update_likes(self):
+        self.post.like_counts = self.liked_by.count()
+        self.save()
+
+class FollowUser(models.Model):
+    user_followed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followed_by')
+    user_following = models.ManyToManyField(User)
+    created_on = models.DateField(auto_now_add=True)
+    updated_on = models.DateField(auto_now=True)

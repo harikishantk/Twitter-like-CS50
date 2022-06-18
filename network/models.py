d55f9d1e7c063321f51ebd13ead9744587cfc391
Harikishan <harikishantk@gmail.com>
2022-06-18 13:06:47 +0530

    git repo setup done

---
+++ network/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    
